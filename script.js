499bbc7cceb0a8738876b185abe597db4c36c424
DunjaDenic <dunjadenic@outlook.com>
2023-05-06 19:13:35 +0200

    Add basic drawing function on mouseover

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function initialize(){
-    const canvasSize=16;
+    const canvasSize=16;//width and height in pixel elements
+    const pixelSize=25; //width and height in actual pixels
+    
     const canvas = document.querySelector("#canvas");
-    
-    
     for (var i=0; i<(canvasSize*canvasSize); i++){
         const pixel = document.createElement("div");
         pixel.classList.add("pixel");
-        canvas.appendChild(pixel);}    
+        canvas.appendChild(pixel);
+        pixel.addEventListener("mouseover", function (e) {color(e.target);});}
+    
+}
+function color(thisDiv){
+    thisDiv.style.backgroundColor="blue";
 }
