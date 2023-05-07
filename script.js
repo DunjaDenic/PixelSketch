e50dbd3eec6dfb3fc606644fb880dbe1407b526d
DunjaDenic <dunjadenic@outlook.com>
2023-05-07 17:27:00 +0200

    Make it possible to enter custom canvas size

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 function initialize(){
-    const canvasSize=16;//width and height in pixel elements
-    const pixelSize=25; //width and height in actual pixels
+    //width and height of the canvas in actual pixels
+    const canvasSizePixels=400;
+    //width and height of the canvas in pixel elements
+    var canvasSizePseudoPixels=16;
+    //width and height of pixel elements in actual pixels
+    var pixelSize=canvasSizePixels/canvasSizePseudoPixels;
+    
     
     const canvas = document.querySelector("#canvas");
-    for (var i=0; i<(canvasSize*canvasSize); i++){
+    createCanvas(canvas, canvasSizePseudoPixels, pixelSize);
+    
+    const btnClear=document.querySelector("#clear-button");
+    btnClear.addEventListener("click", clearCanvas);
+    
+    const numBox=document.querySelector("#numbox");
+    numBox.addEventListener("change", () =>{
+        canvasSizePseudoPixels=numBox.value;
+        pixelSize=canvasSizePixels/canvasSizePseudoPixels;
+        createCanvas(canvas, canvasSizePseudoPixels, pixelSize);})   
+}
+
+function createCanvas(canvas, canvasSizePseudoPixels, pixelSize){
+    //clear existing pixels
+    while (canvas.firstChild) {canvas.removeChild(canvas.lastChild);}
+    //create new pixels
+    for (var i=0; i<(canvasSizePseudoPixels*canvasSizePseudoPixels); i++){
         const pixel = document.createElement("div");
         pixel.classList.add("pixel");
         canvas.appendChild(pixel);
-        pixel.addEventListener("mouseover", function (e) {color(e.target);});}
-    
-}
-function color(thisDiv){
-    thisDiv.style.backgroundColor="blue";
-}
+        pixel.addEventListener("mouseover", function (e) {color(e.target);});
+        pixel.style.height=`${pixelSize}px`; 
+        pixel.style.width=`${pixelSize}px`;}}
+
+function color(thisPixel){
+    thisPixel.style.backgroundColor="blue";}
+
+function clearCanvas() {
+    const pixels=document.querySelectorAll(".pixel");
+    pixels.forEach(pixel=>{pixel.style.backgroundColor="white";})}
+
+//ADD PREVENT RELOAD LATER
