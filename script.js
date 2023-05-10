667e63cbea5fe5faa66d78980292468718c37023
DunjaDenic <dunjadenic@outlook.com>
2023-05-11 01:53:32 +0200

    Make size preset buttons functional, fix some bugs

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ function initialize(){
     var canvasSizePseudoPixels=16;
     //width and height of pixel elements in actual pixels
     var pixelSize=canvasSizePixels/canvasSizePseudoPixels;
-    generateColors();
+    var penColor="blue";
     
     const canvas = document.querySelector("#canvas");
     createCanvas(canvas, canvasSizePseudoPixels, pixelSize);
+    preparePixels(penColor);
+    generateColors();
     
     const btnClear=document.querySelector("#clear-button");
     btnClear.addEventListener("click", clearCanvas);
     
-    const numBox=document.querySelector("#numbox");
-    numBox.addEventListener("change", () =>{
-        canvasSizePseudoPixels=numBox.value;
+    const sizeButtons=document.querySelectorAll(".size");
+    sizeButtons.forEach((button)=>{button.addEventListener("click", (e) => {
+        canvasSizePseudoPixels=button.value;
         pixelSize=canvasSizePixels/canvasSizePseudoPixels;
-        createCanvas(canvas, canvasSizePseudoPixels, pixelSize);})
+        createCanvas(canvas, canvasSizePseudoPixels, pixelSize);
+        preparePixels(penColor);})})
     
-    const pixels=document.querySelectorAll(".pixel");
-    pixels.forEach((pixel)=>{pixel.addEventListener("mouseover", function (e) {
-                                                    color(e.target, penColor);});})
+    const customSize=document.querySelector("#numbox");
+    customSize.addEventListener("change", () =>{
+        if (customSize.value>=5 && customSize.value<=100){
+            canvasSizePseudoPixels=customSize.value;
+            pixelSize=canvasSizePixels/canvasSizePseudoPixels;
+            createCanvas(canvas, canvasSizePseudoPixels, pixelSize);
+            preparePixels(penColor);}
+        else{
+            const warningDiv=document.querySelector("#warning");
+            warningDiv.textContent="Enter a size between 5 and 100!";}})
+    
 }
 
 function createCanvas(canvas, canvasSizePseudoPixels, pixelSize){
@@ -44,9 +55,12 @@ function clearCanvas() {
 
 function generateColors(){
     const colorDivs=document.querySelectorAll(".color");
-    colorDivs.forEach((div)=>{div.style.backgroundColor=div.id})
-}
+    colorDivs.forEach((div)=>{div.style.backgroundColor=div.id})}
 
+function preparePixels(penColor){
+    const pixels=document.querySelectorAll(".pixel");
+    pixels.forEach((pixel)=>{pixel.addEventListener("mouseover", (e) => {
+                                                    color(e.target, penColor);});})}
 
 //ADD PREVENT RELOAD LATER
 //ADD TOGGLE GRID BUTTON
