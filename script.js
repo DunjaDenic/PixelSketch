23609c2bf1c82f8954fa0295557ae63d0c13cb2a
DunjaDenic <dunjadenic@outlook.com>
2023-06-04 22:17:32 +0200

    Make coloring function only fire when both mouseDown and mouseOver events are active

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-function main(){
+var mouseDown=false;
+function main(){   
     //width and height of the canvas in actual pixels
-    const canvasSizePixels=450;
+    const canvasSizePixels=430;
     //width and height of the canvas in pixel elements
     var canvasSizePseudoPixels=18;
     //width and height of pixel elements in actual pixels
     var pixelSize=canvasSizePixels/canvasSizePseudoPixels;
     var canvasColor="white";
     var penColor="#ff595e";
-    
+    var gridOn=false;
+
     const canvas = document.querySelector("#canvas");
     createCanvas(canvas, canvasSizePseudoPixels, pixelSize);
-    preparePixels(penColor);
+    preparePixels(penColor, mouseDown);
     generateColors();
     
-    const btnClear=document.querySelector("#clear-button");
-    btnClear.addEventListener("click", clearCanvas);
+    canvas.addEventListener("mousedown", (e)=>{
+        if (e.buttons==1){mouseDown=true;
+                        console.log("md true");}})
+        
+    canvas.addEventListener("mouseup", (e)=>{mouseDown=false;
+                                                    console.log("md false");})
+        
     
     const sizePresetBtns=document.querySelectorAll(".size-preset");
     sizePresetBtns.forEach(button=>{button.addEventListener("click", () => {
@@ -41,6 +48,19 @@ function main(){
         else{
             penColor=color.id;}
         preparePixels(penColor);})});
+    
+    const colorPicker=document.querySelector(".color-picker");
+    colorPicker.addEventListener("close", () =>{
+        penColor=colorPicker.value;
+        preparePixels(penColor);});
+    Coloris({themeMode:'dark', alpha:false})
+    
+    const toggleGridBtn=document.querySelector("#toggle-grid");
+    toggleGridBtn.addEventListener("click", () => {gridOn=toggleGrid(gridOn)})
+    
+    const clearBtn=document.querySelector("#clear-button");
+    clearBtn.addEventListener("click", clearCanvas);
+    
 }
 
 function createCanvas(canvas, canvasSizePseudoPixels, pixelSize){
@@ -52,14 +72,14 @@ function createCanvas(canvas, canvasSizePseudoPixels, pixelSize){
         pixel.classList.add("pixel");
         canvas.appendChild(pixel);
         pixel.style.height=`${pixelSize}px`; 
-        pixel.style.width=`${pixelSize}px`;}}
+        pixel.style.width=`${pixelSize}px`;
+        pixel.setAttribute("draggable", false);}}
 
 function color(thisPixel, penColor){
     if (penColor!="rainbow"){
         thisPixel.style.backgroundColor=penColor}
     else{
-        thisPixel.style.backgroundColor=`hsl(${getRandomHue()}, 100%, 67%)`;
-    }}
+        thisPixel.style.backgroundColor=`hsl(${getRandomHue()}, 100%, 67%)`;}}
 
 function clearCanvas() {
     const pixels=document.querySelectorAll(".pixel");
@@ -70,11 +90,24 @@ function generateColors(){
     colorDivs.forEach((div)=>{div.style.backgroundColor=div.id})}
 
 function preparePixels(penColor){
+    const canvas = document.querySelector("#canvas");
     const pixels=document.querySelectorAll(".pixel");
-    pixels.forEach((pixel)=>{pixel.addEventListener("mouseover", (e) => {
-                                                    color(e.target, penColor);});})}
+    canvas.addEventListener("mouseover", (e) => {
+        if(mouseDown==true){
+        color(e.target, penColor)}});}
+
 function getRandomHue(){
     return (Math.floor(Math.random() * 357));}
 
+function toggleGrid(gridOn){
+    const pixels=document.querySelectorAll(".pixel");
+    if (gridOn==false){
+        pixels.forEach(pixel=>{pixel.classList.add("grid")});
+        gridOn=true;}
+    else{
+        pixels.forEach(pixel=>{pixel.classList.remove("grid")});
+        gridOn=false;}
+    
+    return gridOn;}
+
 //ADD PREVENT RELOAD LATER
-//ADD TOGGLE GRID BUTTON
